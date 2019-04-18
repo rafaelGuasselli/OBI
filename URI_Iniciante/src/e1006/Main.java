15c524ee68fa9d9b836143f9e152074156ec3812
Rebechi <bibo.rebechi@gmail.com>
2019-04-18 18:28:09 -0300

    e1006
    Resolvido

--- URI_Iniciante/src/e1006/Main.java
+++ URI_Iniciante/src/e1006/Main.java
@@ -1,8 +1,19 @@
 
 package e1006;
 
+import java.util.Scanner;
+//by Gabriel Rebechi
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double A = entrada.nextDouble();
+        double B = entrada.nextDouble();
+        double C = entrada.nextDouble();
         
+        A  = (A*2)/10;
+        B = (B*3)/10;
+        C = (C*5)/10;
+        double res = A+B+C;
+        System.out.printf("MEDIA = %.1f\n",res);
     }
 }
